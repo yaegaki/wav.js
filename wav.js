075952b4884fc631784866133ee755a4c2b8d028
yaegaki <mtaosuke@yahoo.co.jp>
2014-04-28 08:41:34 +0900

    波形合成時の音量調整を追加

--- wav.js
+++ wav.js
@@ -107,17 +107,14 @@ var wav = (function(){
 				var dataArray;
 				var readFunc;
 				if(fmtChunk.bitPerSample === 8){
-					dataArray = Uint8Array;
 					readFunc = reader.readUint8;
 				}else{
-					dataArray = Int16Array;
 					readFunc = reader.readInt16;
 				}
 				chunk.sampleNum = sampleLength;
-				chunk.left = new dataArray(sampleLength);
-				chunk.right = (fmtChunk.channels !== 1)? new dataArray(sampleLength): null;
+				chunk.left = new Int32Array(sampleLength);
+				chunk.right = (fmtChunk.channels !== 1)? new Int32Array(sampleLength): null;
 				for(var i = 0;i < sampleLength;i++){
-
 					chunk.left[i] = readFunc.call(reader);
 					if(fmtChunk.channels !== 1){
 						chunk.right[i] = readFunc.call(reader);
@@ -155,7 +152,7 @@ var wav = (function(){
 			throw 'args error';
 		}
 	}
-	
+
 	/*
 		波形合成
 		pos:開始ミリ秒
@@ -166,19 +163,31 @@ var wav = (function(){
 		var fmt = this.chunks['fmt '];
 		var targetFmt = targetWav.chunks['fmt'];
 		var flag = fmt.channels === targetFmt && fmt.channels === 2;
-		var offset = Math.floor((pos * fmt.channels * fmt.samplingRate * (fmt.bitPerSample / 8)) / 1000) & 0xfffffffe;
+		var offset = Math.floor((pos/1000) * fmt.channels * fmt.samplingRate) & 0xfffffffe;
 
-		for(var i = 0;i < targetWav.chunks.data.sampleNum;i++){
-			if((i+offset) >= this.chunks.data.sampleNum){
-				break;
-			}
-			this.chunks.data.left[i+offset] += targetWav.chunks.data.left[i];
+		var overflow = -1;
+		var overflows = {};
+		var left_buf, right_buf;
+		var length = targetWav.chunks.data.sampleNum+offset;
+		if(length > this.chunks.data.sampleNum){
+			length = this.chunks.data.sampleNum - offset;
+		}else{
+			length = targetWav.chunks.data.sampleNum;
+		}
+
+		for(var i = 0;i < length;i++){
+			var w1 = this.chunks.data.left[i+offset];
+			var w2 = targetWav.chunks.data.left[i];
+			this.chunks.data.left[i+offset] = w1+w2;
 			if(flag){
-				this.chunks.data.right[i+offset] += targetWav.chunks.data.right[i];
+				var w1 = this.chunks.data.right[i+offset];
+				var w2 = targetWav.chunks.data.right[i];
+				this.chunks.data.right[i+offset] = w1+w2;
 			}
 		}
+
 	}
-	
+
 	wav.prototype = {
 		add:addWav
 	};
@@ -192,13 +201,13 @@ var wav = (function(){
 var createWav = (function(){
 	function createWav(samplingRate, millisec){
 		var dataSize = Math.floor(2*samplingRate*(millisec/1000)) & 0xfffffffe;
-		
+
 		var riff = {
 			id:'RIFF',
 			size:dataSize + 36,
 			format:'WAVE'
 		};
-		
+
 		var fmt = {
 			id:'fmt ',
 			size:16,
@@ -209,22 +218,22 @@ var createWav = (function(){
 			blockSize:2,
 			bitPerSample:16
 		};
-		
+
 		var data = {
 			id:'data',
 			size:dataSize,
 			sampleNum:dataSize / 2,
-			left:new Int16Array(dataSize / 2),
+			left:new Int32Array(dataSize / 2),
 			right:null
 		};
-		
+
 		return new wav({
 			'RIFF':riff,
 			'fmt ':fmt,
 			'data':data
 		});
 	}
-	
+
 	return createWav;
 })();
 
@@ -253,17 +262,31 @@ var createWavURL = (function(){
 			write(asciiToBytes('data'));
 			var d = data.chunks['data'];
 			write(Uint32ToBytes(d.size));
+			var rate = 1;
+			var const_max = (fmt.bitPerSample === 8)? 0xff : 0x7fff;
+			var max = const_max;
+			for(var i = 0;i < d.sampleNum;i++){
+				if(d.left[i] > max){
+					max = d.left[i];
+				}
+				if(fmt.channels !== 1){
+					if(d.right[i] > max){
+						max = d.left[i];
+					}
+				}
+			}
+			rate = const_max / max;
 			for(var i = 0;i < d.sampleNum;i++){
 				if(fmt.bitPerSample === 16){
-					write(Int16ToBytes(d.left[i]));
+					write(Int16ToBytes(d.left[i]*rate));
 				}else{
-					write(Uint8ToBytes(d.left[i]));
+					write(Uint8ToBytes(d.left[i]*rate));
 				}
 				if(fmt.channels !== 1){
 					if(fmt.bitPerSample === 16){
-						write(Int16ToBytes(d.right[i]));
+						write(Int16ToBytes(d.right[i]*rate));
 					}else{
-						write(Uint8ToBytes(d.right[i]));
+						write(Uint8ToBytes(d.right[i]*rate));
 					}
 				}
 			}
